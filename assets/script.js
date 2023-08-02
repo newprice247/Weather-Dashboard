2afb5272a933100e0ce1aba866ce8ed947ca5092
newprice247 <newprice247@gmail.com>
2023-08-02 11:58:47 -0400

    add styling and start button interactions

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,28 @@
 var fiveDayForecast = $('.day')
-
 var getWeather = () => {
+    // $.each(recentSearches, (i,val) => {
+    //     $('#recentSearches').append(`<div>${val}</div>`)
+    //     console.log(recentSearches)
+    // })
 
-    var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q={32935}&limit={limit}&appid={4ab8fb0f640052a7c1fc096698340e94}`;
+    $('#search').on('click', function () {
+        var locationSearch = $('.input').val();
+        localStorage.setItem(locationSearch, locationSearch)
+    })
 
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data)
-        })
+    // var getRecentSearches = () => {
+    //     $.each()
+    // }
+
+    // var requestUrl = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={4ab8fb0f640052a7c1fc096698340e94}`;
+
+    // fetch(requestUrl)
+    //     .then(function (response) {
+    //         return response.json()
+    //     })
+    //     .then(function (data) {
+    //         console.log(data)
+    //     })
 }
 
 getWeather()
