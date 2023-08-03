5933568b0d881f6038da7c07b8fb330dfb2ff513
newprice247 <newprice247@gmail.com>
2023-08-03 13:48:19 -0400

    add enter key press and welcome message

--- assets/script.js
+++ assets/script.js
@@ -8,55 +8,82 @@ var getWeather = () => {
     //     $('#recentSearches').append(`<div>${val}</div>`)
     //     console.log(recentSearches)
     // })
-
+    $("#searchBar").keypress(function (event) {
+        if (event.keyCode === 13) {
+            $("#search").click();
+        }
+    });
+    
     $('#search').on('click', function () {
-        var locationSearch = $('.input').val();
-        localStorage.setItem(locationSearch, locationSearch)
+        $('.fiveDay').html(``)
+        $('.fiveDay').addClass('box')
+        var city = $('.input').val()
+
+        var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?&q=${city}&units=imperial&cnt=&appid=4ab8fb0f640052a7c1fc096698340e94`;
+
+        fetch(requestUrl)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                var dt = data.list[0].dt;
+                var day = new Date(dt * 1000);
+                var pop = data.list[0].pop;
+                var chanceOfRain = Math.floor(pop * 100);
+                var temp = data.list[0].main.temp
+                $('.todaysForecast').html(`
+                <p id="today" class="has-text-primary-dark">Today's Forecast for ${data.city.name},${data.city.country}</p>
+                <div class = ml-6>
+                    <p>${data.list[0].weather[0].main}</p>
+                    <p>Temp: ${Math.floor(temp)}°F</p>
+                    <p>Wind Speed: ${data.list[0].wind.speed}</p>
+                    <p>Chance of Rain: ${chanceOfRain}%</p>
+                </div>
+            `)
+
+                for (let i = 4; i < data.list.length; i += 8) {
+                    console.log(data.list) //dates
+                    var dt = data.list[i].dt;
+                    var day = new Date(dt * 1000);
+                    var pop = data.list[i].pop;
+                    var chanceOfRain = Math.floor(pop * 100);
+                    var temp = data.list[i].main.temp
+
+                    $('.fiveDay').append(`
+                <div class="column m-3 has-text-centered day">
+                        <p class="is-size-5 mb-3 has-text-primary-dark">${day.toDateString()}</p>
+                        <div class='box'>
+                            <p>${data.list[i].weather[0].main}</p>
+                            <p>Temp: ${Math.floor(temp)}°F</p>
+                            <p>Wind Speed: ${data.list[i].wind.speed}</p>
+                            <p>Chance of Rain: ${chanceOfRain}%</p>
+                        </div>
+                </div>`)
+                }
+            })
     })
 
     // var getRecentSearches = () => {
     //     $.each()
     // }
 
-    var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?&q=32935&units=imperial&cnt=&appid=4ab8fb0f640052a7c1fc096698340e94`;
 
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            var dt = data.list[0].dt;
-            var day = new Date(dt*1000);
-            var pop = data.list[0].pop;
-            var chanceOfRain = Math.floor(pop * 100);
-            $('.todaysForecast').append(`
-                <p>Today's Forecast for ${data.city.name}</p>
-                <div class = ml-6>
-                    <p>${data.list[0].weather[0].main}</p>
-                    <p>Temp: ${data.list[0].main.temp}</p>
-                    <p>Wind Speed: ${data.list[0].wind.speed}</p>
-                    <p>Chance of Rain: ${chanceOfRain}%</p>
-                </div>
-            `)
-
-            for (let i = 4; i < data.list.length; i+=8) {
-            console.log(data.list) //dates
-            var dt = data.list[i].dt;
-            var day = new Date(dt*1000);
-            var pop = data.list[i].pop;
-            var chanceOfRain = Math.floor(pop * 100);
-            $('.fiveDay').append(`
-                <div class="column m-3 has-text-centered day">
-                        <p class="is-size-5 mb-3">${day.toDateString()}</p>
-                        <div class='box'>
-                            <p>${data.list[i].weather[0].main}</p>
-                            <p>Temp: ${data.list[i].main.temp}</p>
-                            <p>Wind Speed: ${data.list[i].wind.speed}</p>
-                            <p>Chance of Rain: ${chanceOfRain}%</p>
-                        </div>
-                </div>`)
-            }
-        })
 }
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+}
+
+$('#r').on('click', getLocation())
 
 getWeather()
