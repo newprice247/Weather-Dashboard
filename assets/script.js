c7935698749129c0217cc1c5b0dcc150a6c247ae
newprice247 <newprice247@gmail.com>
2023-08-03 12:18:39 -0400

    five day forecast implemented and gifs added to assets

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,8 @@
 var fiveDayForecast = $('.day')
+var today = new Date()
+$('#header').append(`
+
+    <p class="is-size-4">${today.toDateString()}</p>`)
 var getWeather = () => {
     // $.each(recentSearches, (i,val) => {
     //     $('#recentSearches').append(`<div>${val}</div>`)
@@ -14,15 +18,45 @@ var getWeather = () => {
     //     $.each()
     // }
 
-    // var requestUrl = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={4ab8fb0f640052a7c1fc096698340e94}`;
+    var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?&q=32935&units=imperial&cnt=&appid=4ab8fb0f640052a7c1fc096698340e94`;
 
-    // fetch(requestUrl)
-    //     .then(function (response) {
-    //         return response.json()
-    //     })
-    //     .then(function (data) {
-    //         console.log(data)
-    //     })
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            var dt = data.list[0].dt;
+            var day = new Date(dt*1000);
+            var pop = data.list[0].pop;
+            var chanceOfRain = Math.floor(pop * 100);
+            $('.todaysForecast').append(`
+                <p>Today's Forecast for ${data.city.name}</p>
+                <div class = ml-6>
+                    <p>${data.list[0].weather[0].main}</p>
+                    <p>Temp: ${data.list[0].main.temp}</p>
+                    <p>Wind Speed: ${data.list[0].wind.speed}</p>
+                    <p>Chance of Rain: ${chanceOfRain}%</p>
+                </div>
+            `)
+
+            for (let i = 4; i < data.list.length; i+=8) {
+            console.log(data.list) //dates
+            var dt = data.list[i].dt;
+            var day = new Date(dt*1000);
+            var pop = data.list[i].pop;
+            var chanceOfRain = Math.floor(pop * 100);
+            $('.fiveDay').append(`
+                <div class="column m-3 has-text-centered day">
+                        <p class="is-size-5 mb-3">${day.toDateString()}</p>
+                        <div class='box'>
+                            <p>${data.list[i].weather[0].main}</p>
+                            <p>Temp: ${data.list[i].main.temp}</p>
+                            <p>Wind Speed: ${data.list[i].wind.speed}</p>
+                            <p>Chance of Rain: ${chanceOfRain}%</p>
+                        </div>
+                </div>`)
+            }
+        })
 }
 
 getWeather()
